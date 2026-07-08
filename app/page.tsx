27e6a21bba9957b250714f31e63b44c2c6496cc5
lilKriT <lilKriTwebDev@gmail.com>
2026-07-09 01:46:45 +0200

    Lots of styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <section className="min-h-dvh flex bg-[url('/bg-pattern.svg')] bg-size-[100%_50%] bg-no-repeat justify-center items-center">
-      <div className="">
+      <div className="w-full flex flex-col justify-center items-center">
         {/* Heading */}
-        <p>Simple, traffic-based pricing</p>
-        <p>Sign-up for our 30-day trial. No credit card required. </p>
+        <div className="flex flex-col gap-8 bg-[url(/pattern-circles.svg)] bg-no-repeat bg-size-[auto_90%] bg-center pt-16 pb-16">
+          <h1 className="text-center text-5xl text-text-dark font-extrabold">
+            Simple, traffic-based pricing
+          </h1>
+          <p className="text-center text-2xl font-extrabold">
+            Sign-up for our 30-day trial. No credit card required.{" "}
+          </p>
+        </div>
 
         {/* Card */}
-        <div className="bg-pricing-background rounded-lg shadow-xl">
+        <div className="w-full max-w-5xl bg-pricing-background rounded-lg shadow-xl">
           {/* Upper section */}
           <div>
             <p>Pageviews $ /month</p>
@@ -16,11 +24,22 @@ export default function Home() {
           </div>
 
           {/* Sub-section */}
-          <div className="border-t border-black p-12">
-            <p>Unlimited websites</p>
-            <p>100% data ownership</p>
-            <p>Email reports</p>
-            <p>Start my trial</p>
+          <div className="border-t border-black p-12 grid grid-cols-2 gap-8">
+            <div>
+              <ul className="list-image-[url(/icon-check.svg)] checkmarkList">
+                <li>Unlimited websites</li>
+                <li>100% data ownership</li>
+                <li>Email reports</li>
+              </ul>
+            </div>
+            <div className="flex justify-center items-center">
+              <Link
+                href={"#"}
+                className="bg-cta-background text-cta-text rounded-full px-8 py-2 font-extrabold"
+              >
+                Start my trial
+              </Link>
+            </div>
           </div>
         </div>
       </div>
