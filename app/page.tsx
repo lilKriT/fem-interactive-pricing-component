54fac649d4549278b017971c4d8aa512d9358fd2
lilKriT <lilKriTwebDev@gmail.com>
2026-07-11 18:40:57 +0200

    Styling.

--- app/page.tsx
+++ app/page.tsx
@@ -18,24 +18,45 @@ export default function Home() {
         {/* Card */}
         <div className="w-full max-w-3xl bg-pricing-background rounded-lg shadow-xl">
           {/* Upper section */}
-          <div>
+          <div className="flex flex-col justify-center items-center gap-12 p-12">
             {/* Top Row */}
-            <div className="flex justify-between items-center">
-              <p>100k Pageviews</p>
-              <p>$16 /month</p>
+            <div className="w-full flex justify-between items-center">
+              <p className="uppercase font-extrabold tracking-[.15rem] text-lg">
+                100k Pageviews
+              </p>
+              <p className="font-extrabold text-xl flex justify-center items-center gap-2">
+                <span className="font-extrabold text-slate-800 text-5xl">
+                  ${"16.00"}
+                </span>{" "}
+                / month
+              </p>
             </div>
 
             {/* Range */}
             <Range />
 
             {/* Bottom Row */}
-            <div>Monthly Billing Yearly Billing 25% discount</div>
+            <div className="grid grid-cols-[1fr_auto_1fr] gap-8 font-extrabold ">
+              {/* Grid is probably the best way to center the button */}
+              <div>
+                <p>Monthly Billing</p>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+              </div>
+              <div className="flex justify-center items-center gap-4">
+                <p>Yearly Billing</p>{" "}
+                <p className="bg-discount-background text-discount-text rounded-full px-2 py-1 text-xs">
+                  25% discount
+                </p>
+              </div>
+            </div>
           </div>
 
           {/* Sub-section */}
-          <div className="border-t border-black p-12 grid grid-cols-2 gap-8">
+          <div className="border-t border-slate-400/30 p-12 grid grid-cols-2 gap-8">
             <div>
-              <ul className="list-image-[url(/icon-check.svg)] checkmarkList">
+              <ul className="list-image-[url(/icon-check.svg)] ml-4 checkmarkList flex flex-col gap-2">
                 <li>Unlimited websites</li>
                 <li>100% data ownership</li>
                 <li>Email reports</li>
