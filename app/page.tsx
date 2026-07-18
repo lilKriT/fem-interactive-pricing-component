1d1197d30ec4c1284065b121e6b006d5373c3d1b
lilKriT <lilKriTwebDev@gmail.com>
2026-07-18 02:33:50 +0200

    Working on responsiveness.

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,20 @@ import PricingCalculator from "./_components/PricingCalculator";
 export default function Home() {
   return (
     <section className="min-h-dvh flex bg-[url('/bg-pattern.svg')] bg-size-[100%_50%] bg-no-repeat justify-center items-start">
-      <div className="w-full flex flex-col gap-16 justify-start items-center px-0 py-20">
+      <div className="w-full flex flex-col gap-16 justify-start items-center py-20">
         {/* Heading */}
-        <div className="flex flex-col gap-8 bg-[url(/pattern-circles.svg)] bg-no-repeat bg-size-[auto_90%] bg-center py-20">
-          <h1 className="text-center text-5xl text-text-dark font-extrabold">
+        <div className="flex flex-col gap-8 bg-[url(/pattern-circles.svg)] bg-no-repeat bg-size-[auto_90%] bg-center px-4 py-20">
+          <h1 className="text-center text-[clamp(1rem,5vw,3rem)] text-text-dark font-extrabold">
             Simple, traffic-based pricing
           </h1>
-          <p className="text-center text-2xl font-extrabold">
-            Sign-up for our 30-day trial. No credit card required.
+          <p className="text-center text-[clamp(0.875rem,3vw,1.5rem)] font-extrabold">
+            <span className="text-nowrap">Sign-up for our 30-day trial.</span>{" "}
+            <span className="text-nowrap">No credit card required.</span>
           </p>
         </div>
 
         {/* Card */}
-        <div className="w-full max-w-3xl bg-pricing-background rounded-lg shadow-xl">
+        <div className="max-w-3xl bg-pricing-background rounded-lg shadow-xl mx-12">
           {/* Pricing section */}
           <PricingCalculator />
 
